da70ca3ca76fb4fe0f004aea83ac2cf859d2200f
Soltanbkz <soltanbkzbek@gmail.com>
2024-07-17 23:30:36 +0600

    com

--- config/settings.py
+++ config/settings.py
@@ -164,8 +164,7 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-# https://docs.djangoproject.com/en/dev/ref/settings/#static-root
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = '/root/code/platform/static/'
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",

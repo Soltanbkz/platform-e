6169d19b4cd7f84f8ba1c527375afe14e0539c6a
Soltanbkz <soltanbkzbek@gmail.com>
2024-07-17 23:12:49 +0600

    com

--- config/settings.py
+++ config/settings.py
@@ -165,13 +165,12 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-root
-STATIC_ROOT = '/root/code/platform/staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
-
 
 # Media files config
 MEDIA_URL = "/media/"

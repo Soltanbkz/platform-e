b8b19825efabc792e607ac4cd02ff9b2289a961f
Soltanbkz <soltanbkzbek@gmail.com>
2024-07-19 21:19:35 +0600

    kpfdo

--- config/settings.py
+++ config/settings.py
@@ -1,7 +1,5 @@
 import os
 from decouple import config
-import environ
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
